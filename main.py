f26f1adcf6cf699c773961cc79bdf162a2c9e91d
Selfcod <brkyozdemir-06@outlook.com>
2023-11-21 22:38:31 +0300

    instruction has defined.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from moviepy.editor import *
 
 videom = VideoFileClip("Kurbağa.mp4")
-a = videom.subclip(t_start=2)
+a = videom.subclip(t_start=2) # The int value represents start time in seconds
 a.write_videofile("denedim.mp4")
 
 
